ac0fd9bddb524d18bbcf8391680b35ed1377d182
rts-pyers <jonathan.pyers@rtslabs.com>
2025-07-12 21:15:04 -0400

    chore: move applyPatch export to /client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,6 @@
 export { MeshClient, Status } from "./client";
 export { Connection } from "./connection";
 export { CodeError } from "../common/codeerror";
+
+import fjp from "fast-json-patch";
+export const { applyPatch } = fjp;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export { MeshClient } from "./client";
 export { MeshServer, type MeshContext, type SocketMiddleware } from "./server";
 export { type CodeError } from "./common/codeerror";
-
-import fjp from "fast-json-patch";
-export const { applyPatch } = fjp;
